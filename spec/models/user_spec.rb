d7f909677555f2e7ff999bdd17502402f034ca8d
Dominic McKelvey <dominic.mckelvey@navy.mil>
2014-04-14 09:58:56 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User do
   it { should respond_to(:password)}
   it { should respond_to(:password_confirmation)}
   it { should respond_to(:authenticate)}
+  it { should respond_to(:remember_token) }
   it { should be_valid }
 
   describe "when name is not present" do
@@ -63,12 +64,12 @@ describe "when email address is already taken" do
   end
 
   describe "when password is not present" do
-  before do
-    @user = User.new(name: "Example User", email: "user@example.com",
+    before do
+     @user = User.new(name: "Example User", email: "user@example.com",
                      password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
   end
-  it { should_not be_valid }
-end
 
 describe "when password doesn't match confirmation" do
   before { @user.password_confirmation = "mismatch" }
@@ -96,4 +97,8 @@ describe "return value of authenticate method" do
   end
 end
 
+ describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
